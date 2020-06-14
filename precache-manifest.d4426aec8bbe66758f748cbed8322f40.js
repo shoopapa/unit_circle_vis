3bfe5234acbaa3d322c6e5a4a6b13d6cdc704ba8
shoopapa <joedavis29@gmail.com>
2020-06-14 18:51:25 -0400

    Updates

---
+++ precache-manifest.d4426aec8bbe66758f748cbed8322f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1c8cf1e2889216197db57db091173c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af28a80606b2b43be673",
+    "url": "/static/css/main.8edac149.chunk.css"
+  },
+  {
+    "revision": "d33223be10452daa6818",
+    "url": "/static/js/2.34feb194.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.34feb194.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af28a80606b2b43be673",
+    "url": "/static/js/main.8571ab49.chunk.js"
+  },
+  {
+    "revision": "c1182fe2f23700bc7070",
+    "url": "/static/js/runtime-main.b5bb424c.js"
+  }
+]);
